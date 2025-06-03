484f3a3dcbb34df290a2f11caafc37110c479ef6
teachVN <vinnap1976@gmail.com>
2025-06-03 14:15:58 +0200

    third commit

---
+++ backend/u5w1d1teoria/src/main/java/it/epicode/u5w1d1teoria/U5w1d1teoriaApplication.java
@@ -0,0 +1,20 @@
+package it.epicode.u5w1d1teoria;
+
+import it.epicode.u5w1d1teoria.bean.Drink;
+import it.epicode.u5w1d1teoria.bean.Topping;
+import it.epicode.u5w1d1teoria.configuration.AppConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class U5w1d1teoriaApplication {
+
+	public static void main(String[] args) {
+		//istruzione da non cancellare perchè serve per startare l'applicazione
+		SpringApplication.run(U5w1d1teoriaApplication.class, args);
+
+
+	}
+
+}
