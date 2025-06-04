ff83e5b6608271268c0709bd2a9909bbbfb5acce
teachVN <vinnap1976@gmail.com>
2025-06-04 17:45:58 +0200

    u5w1d3pratica

--- backend/u5w1d2pratica/src/test/java/it/epicode/u5w1d2pratica/U5w1d2praticaApplicationTests.java
+++ backend/u5w1d2pratica/src/test/java/it/epicode/u5w1d2pratica/U5w1d2praticaApplicationTests.java
@@ -1,15 +1,14 @@
 package it.epicode.u5w1d2pratica;
 
-import it.epicode.u5w1d2pratica.bean.Drink;
-import it.epicode.u5w1d2pratica.bean.Menu;
-import it.epicode.u5w1d2pratica.bean.Pizza;
-import it.epicode.u5w1d2pratica.bean.Tavolo;
+import it.epicode.u5w1d2pratica.bean.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 @SpringBootTest
 class U5w1d2praticaApplicationTests {
@@ -71,6 +70,48 @@ class U5w1d2praticaApplicationTests {
 		Menu menu = ctx.getBean(Menu.class);
 		long count = menu.getProdotti().stream().filter(prodotto -> prodotto.getNome().equalsIgnoreCase(topping)).count();
 		Assertions.assertEquals(atteso, count);
+	}
+	@Test
+	public void verificaPrezzoMargherita(){
+		Pizza margherita = ctx.getBean("margherita", Pizza.class);
+
+		Assertions.assertEquals(5, margherita.getPrezzo());
+	}
+	@Test
+	public void verificaTotaleOrdine(){
+		Tavolo t1 = ctx.getBean("t1", Tavolo.class);
+
+		Ordine o1 = new Ordine();
+		o1.setNumCoperti(2);
+		o1.setTavolo(t1);
+		o1.setProdotti(List.of(ctx.getBean("cocaCola", Drink.class),
+				ctx.getBean("cocaCola", Drink.class),
+				ctx.getBean("margherita", Pizza.class),
+				ctx.getBean("primavera", Pizza.class)));
+
+		Assertions.assertEquals(21, o1.totaleOrdine());
+	}
+
+	@Test
+	public void verificaNumProdottiMenu(){
+		Menu menu = ctx.getBean(Menu.class);
+
+		Assertions.assertEquals(7,menu.getProdotti().size());
+	}
+	@ParameterizedTest
+	@CsvSource({"t1,3", "t2,2", "t3,0"})
+	public void verificaCostoCopertoTavoli(String tavolo, double costoCoperto){
+		Tavolo t = ctx.getBean(tavolo,Tavolo.class);
+
+		Assertions.assertEquals(costoCoperto,t.getCostoCoperto());
+	}
+
+	@ParameterizedTest
+	@CsvSource({"cocaCola,2", "water,1"})
+	public void verificaPrezziDrink(String nomeDrink, int prezzo){
+		Drink drink = ctx.getBean(nomeDrink, Drink.class);
+
+		Assertions.assertEquals(prezzo,drink.getPrezzo());
 	}
 
 
