ca5203e455b3371c0e5a5e367a24ad38ba34c76d
teachVN <vinnap1976@gmail.com>
2025-06-05 11:41:32 +0200

    u5w1d4teoria

--- backend/u5w1d4teoria/src/main/java/it/epicode/u5w1d4teoria/Runner.java
+++ backend/u5w1d4teoria/src/main/java/it/epicode/u5w1d4teoria/Runner.java
@@ -6,33 +6,55 @@ import it.epicode.u5w1d4teoria.entities.Universita;
 import it.epicode.u5w1d4teoria.repository.StudenteRepository;
 import it.epicode.u5w1d4teoria.repository.UniversitaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Runner implements CommandLineRunner {
+    /*
+    ogni volta che vogliamo usare i repository dobbiamo iniettarli nella classe che li
+    deve usare, iniettarli con l'autowired
+     */
+
     @Autowired
     private StudenteRepository studenteRepository;
+
     @Autowired
     private UniversitaRepository universitaRepository;
+
+    @Autowired
+    @Qualifier("Mario")
+    private Studente s1;
+
+    @Autowired
+    @Qualifier("Francesca")
+    private Studente s2;
+
+    @Autowired
+    @Qualifier("Roberto")
+    private Studente s3;
+
+    @Autowired
+    private Universita universita;
 
     @Override
     public void run(String... args) throws Exception {
 
-        AnnotationConfigApplicationContext ctx=
-                new AnnotationConfigApplicationContext(AppConfig.class);
+//        AnnotationConfigApplicationContext ctx=
+//                new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Studente s1 = ctx.getBean("Mario", Studente.class);
+        //Studente s1 = ctx.getBean("Mario", Studente.class);
         studenteRepository.save(s1);
 
-        Studente s2 = ctx.getBean("Francesca", Studente.class);
+        //Studente s2 = ctx.getBean("Francesca", Studente.class);
         studenteRepository.save(s2);
 
-        Studente s3 = ctx.getBean("Roberto", Studente.class);
+        //Studente s3 = ctx.getBean("Roberto", Studente.class);
         studenteRepository.save(s3);
 
-        Universita universita = ctx.getBean(Universita.class);
+        //Universita universita = ctx.getBean(Universita.class);
         universitaRepository.save(universita);
 
         System.out.println(universita);
--- backend/u5w1d4teoria/src/main/java/it/epicode/u5w1d4teoria/repository/StudenteRepository.java
+++ backend/u5w1d4teoria/src/main/java/it/epicode/u5w1d4teoria/repository/StudenteRepository.java
@@ -2,6 +2,7 @@ package it.epicode.u5w1d4teoria.repository;
 
 import it.epicode.u5w1d4teoria.entities.Studente;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//il repository deve essere una interfaccia, deve estendere JpaRepository
+//i tipi tra parentesi angolari sono il nome della entità e il tipo della chiave primaria
 public interface StudenteRepository extends JpaRepository<Studente,Integer> {
 }
