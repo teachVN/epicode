c63155e0b4c8d11242c23b68f8e679a02075c176
teachVN <vinnap1976@gmail.com>
2025-06-12 18:07:10 +0200

    u5w2d4pratica

---
+++ backend/u5w2d4pratica/src/main/java/it/epicode/u5w2d4pratica/U5w2d4praticaApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.u5w2d4pratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w2d4praticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w2d4praticaApplication.class, args);
+	}
+
+}
